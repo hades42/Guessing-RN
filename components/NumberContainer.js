12fc2a500623afb566a22b8962bebb1fd0d4ea8a
Van Nguyen Nguyen <nguyenvannguyen.oc@gmail.com>
2022-04-15 11:55:57 +1000

    Number container component
    Just for better visualization

---
+++ components/NumberContainer.js
@@ -0,0 +1,30 @@
+import { View, Text, StyleSheet } from "react-native";
+
+import Colors from "../constants/colors";
+
+const NumberContainer = ({ children }) => {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.numberText}>{children}</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        borderWidth: 4,
+        borderColor: Colors.accent500,
+        padding: 24,
+        borderRadius: 8,
+        margin: 24,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    numberText: {
+        color: Colors.accent500,
+        fontSize: 35,
+        fontWeight: "bold",
+    },
+});
+
+export default NumberContainer;
